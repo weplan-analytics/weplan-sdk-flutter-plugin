bf655fe0179cabfae8f9a1380ebc0ba6c1603144
Gerard Cuscó <gerard.cusco@weplananalytics.com>
2023-07-12 11:19:11 +0200

    change imports

--- android/src/main/kotlin/com/cumberland/weplan_sdk/WeplanSdkPlugin.kt
+++ android/src/main/kotlin/com/cumberland/weplan_sdk/WeplanSdkPlugin.kt
@@ -3,8 +3,8 @@ package com.cumberland.weplan_sdk
 import android.app.Activity
 import android.content.Context
 import androidx.annotation.NonNull;
-import com.cumberland.weplansdk.WeplanSdkInit;
-import com.cumberland.weplansdk.WeplanSdk.NotificationController;
+import com.cumberland.utils.init.Weplan;
+import com.cumberland.utils.init.Weplan.NotificationController;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
